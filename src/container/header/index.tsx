a0bd935ebb3cc8e90bf13f341c1ae6f62636221c
SivashankaranK <sivashankaran.K1999@gmail.com>
2023-03-07 16:33:00 +0530

    burger menu implementation for responsive views

--- src/container/header/index.tsx
+++ src/container/header/index.tsx
@@ -5,10 +5,13 @@ import logo from '../../assets/icons/logo.svg';
 import arrowIcon from '../../assets/icons/arrow.svg';
 import searchIcon from '../../assets/icons/search.svg';
 import headphonesIcon from '../../assets/icons/headphones.svg';
+import phoneIcon from '../../assets/icons/phone.svg';
 import cartIcon from '../../assets/icons/cart.svg';
 import heartIcon from '../../assets/icons/heart.svg';
 import userIcon from '../../assets/icons/user.svg';
 import menuIcon from '../../assets/icons/menu-burger.svg';
+import signInIcon from '../../assets/icons/sign-in.svg';
+
 import { createSignal } from 'solid-js';
 import { Offcanvas } from 'solid-bootstrap';
 
@@ -84,22 +87,40 @@ export const Header = () => {
         placement={'end'}
       >
         <Offcanvas.Header closeButton>
-          <Offcanvas.Title />
+          <Offcanvas.Title>ORDER DETAILS</Offcanvas.Title>
         </Offcanvas.Header>
-        <Offcanvas.Body>
-          {headerOrderList.map((it) => {
-            return (
-              <div class='burgger_contents'>
-                <span class='burgger_icon_content'>
-                  <img src={it.darkLogo || it.logo} alt={it.value} class='burgger_icon'/>
-                </span>
-                <span class='burgger_label'>
-                  {it.label}
-                </span>
-                <span class='seperator' />
-              </div>
-            )
-          })}
+        <Offcanvas.Body class='burger_container'>
+          <div>
+            {headerOrderList.map((it) => {
+              return (
+                <div class='burgger_contents'>
+                  {/* <span class='burgger_icon_content'>
+                  <img src={it.darkLogo || it.logo} alt={it.value} class='burgger_icon' />
+                </span> */}
+                  <span class='burgger_label'>
+                    {it.label}
+                  </span>
+                </div>
+              )
+            })}
+          </div>
+          <div class='footer_content'>
+            <div class='contact_details'>
+              <span class='label'>
+                Call Us Now
+              </span>
+              <span class='phone_number'>
+                <span>+011 5827918</span>
+                <img src={phoneIcon} alt='headphonesIcon' class='icon' />
+              </span>
+            </div>
+            <div class='sign_in'>
+              <span>
+                Sign In
+              </span>
+              <img src={signInIcon} alt='signInIcon' class='icon' />
+            </div>
+          </div>
         </Offcanvas.Body>
       </Offcanvas>
     </div >
