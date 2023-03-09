f120a87185b19368dfee3dff069e27aab27b6f7a
SivashankaranK <sivashankaran.K1999@gmail.com>
2023-03-09 13:58:23 +0530

    sub nav prop list

--- src/utils/constant/index.ts
+++ src/utils/constant/index.ts
@@ -4,8 +4,19 @@ import flagIcon from '../../assets/icons/flag.svg'
 import darkTruckIcon from '../../assets/icons/dark_truck.svg'
 import darkRepeatIcon from '../../assets/icons/dark_repeat.svg'
 
+import twitterIcon from '../../assets/icons/twitter.svg'
+import fbIcon from '../../assets/icons/fb.svg'
+import linkedinIcon from '../../assets/icons/linkedin.svg'
+import instagramIcon from '../../assets/icons/instagram.svg'
+
+
 export const headerOrderList = [
   { logo: truckIcon, label: 'Orders & Tracking', value: 'order', darkLogo: darkTruckIcon },
   { logo: flagIcon, label: 'Delivery within India', value: 'delivery' },
   { logo: repeatIcon, label: 'Return', value: 'return', darkLogo: darkRepeatIcon },
 ]
+
+export const subHeaderList = {
+  leftNav: [{ label: 'Home' }, { label: 'Easy Monthly Installments' }, { label: 'Shop by Brands' }, { label: 'Become a Vendor' }],
+  rightNav: [{ icon: fbIcon }, { icon: twitterIcon }, { icon: linkedinIcon }, { icon: instagramIcon }]
+}
